a731d85d0c7dd13d4fc872163a42924e2e264517
Administrator <Administrator@DESKTOP-I3ADRIV>
2024-01-28 00:28:51 +0300

    ModelMetadataType yöntemi ile validasyonu farklı bir sınıfa yüklemek

---
+++ ExampleCore2/Models/ModelsMetadataTypes/ProductMetadata.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExampleCore2.Models.ModelMetadataTypes
+{
+    public class ProductMetadata
+    {
+        [Required(ErrorMessage = "Lütfen Değeri Gir")]
+        public int id { get; set; }
+        [StringLength(10, ErrorMessage = "10'dan fazla girme gardaş")]
+        public string description { get; set; }
+        [EmailAddress(ErrorMessage = "E-mail Gir")]
+        public string category { get; set; }
+    }
+}
--- ExampleCore2/Models/Product.cs
+++ ExampleCore2/Models/Product.cs
@@ -1,17 +1,19 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using ExampleCore2.Models.ModelMetadataTypes;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace ExampleCore2.Models
 {
+    [ModelMetadataType(typeof(ProductMetadata))]//Başka bir sınıfa validasyon olayını yüklemiş olduk
     public class Product
     {
         //Aşağıdaki [] içinde annotation ile istediğiniz doğrulama kontrolü yapılıyor.Yukarıdaki sınıf da mutlaka eklenmesi gerekiyor.
-        [Required( ErrorMessage ="Lütfen Değeri Gir")]
+        //[Required( ErrorMessage ="Lütfen Değeri Gir")]
         public int id { get; set; }
-        
         public string name { get; set; }
-        [StringLength(10,ErrorMessage ="10'dan fazla girme gardaş")]
+        //[StringLength(10,ErrorMessage ="10'dan fazla girme gardaş")]
         public string description { get; set; }
-        [EmailAddress(ErrorMessage ="E-mail Gir")]
+        //[EmailAddress(ErrorMessage ="E-mail Gir")]
         public string category { get; set; }
     }
 }
