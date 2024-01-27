7b154364cbad0a75d50160d4acbed91a82d897dd
Administrator <Administrator@DESKTOP-I3ADRIV>
2024-01-27 23:13:10 +0300

    Add project files.

---
+++ ExampleCore2/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ExampleCore2.Models;
+namespace ExampleCore2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+           
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Product product)
+        {
+            ViewBag.id = product.id;
+            ViewBag.name = product.name;
+            ViewBag.description = product.description;
+            ViewBag.category = product.category;
+            return View();
+        }
+    }
+}
---
+++ ExampleCore2/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExampleCore2.Models
+{
+    public class Product
+    {
+        //Aşağıdaki [] içinde annotation ile istediğiniz doğrulama kontrolü yapılıyor.Yukarıdaki sınıf da mutlaka eklenmesi gerekiyor.
+        [Required( ErrorMessage ="Lütfen Değeri Gir")]
+        public int id { get; set; }
+        
+        public string name { get; set; }
+        [StringLength(10,ErrorMessage ="10'dan fazla girme gardaş")]
+        public string description { get; set; }
+        [EmailAddress(ErrorMessage ="E-mail Gir")]
+        public string category { get; set; }
+    }
+}
